90c7eb5ba3052e98e92dfed9c85c5483c9f4c356
Viglitch <varvarafeofanova49@gmail.com>
2025-11-17 15:19:35 +0300

    Logic and structure

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,25 @@
+import java.util.List;
+import java.util.Queue;
+
 public class Book {
+    int bookID;
+    String title;
+    String author;
+    boolean isAvailable;
+    Queue<Application> applicationBuffer;
+    //TODO: какая именно очередь?
+
+    public void addToBuffer(Application app){
+        applicationBuffer.add(app);
+    }
+
+    public void removeFromBuffer(int id){
+        //TODO: удаляет заявку с данным id
+    }
+
+    public List<Application> sortApplications(){
+        List<Application> res;
+        //TODO: отсортировать по приоритету
+        return res;
+    }
 }
--- src/main/java/BookShielf.java
+++ src/main/java/BookShielf.java
@@ -1,2 +1,27 @@
+import java.util.List;
+import java.util.Map;
+
 public class BookShielf {
+    Map<Integer, Book > AllBooks;
+    List<Reader> Readers;
+    List<Librarian> Librarians;
+
+    public void addBook(Book newBook){
+        AllBooks.put(AllBooks.size(), newBook);
+    }
+
+    public void registerReader(Reader newReader){
+        Readers.add(newReader);
+    }
+
+    public boolean processApplication(Application myApp){
+        //TODO: логика заявок
+        return true;
+    }
+
+    public List<Book> getAvailableBooks(){
+        List<Book> res;
+        //TODO: только свободные
+        return res;
+    }
 }
